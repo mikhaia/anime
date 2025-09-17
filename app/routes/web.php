876c084abeed6da2c3d1d15b971ae9d6959cc170
Alexander Mikhailov <45555229+mikhaia@users.noreply.github.com>
2025-09-17 19:02:11 +0700

    Refine favorites schema to reference anime records

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Support\Auth;
 use Illuminate\Http\Request;
 
 /** @var \Laravel\Lumen\Routing\Router $router */
@@ -26,7 +27,23 @@ $router->get('/', function () {
 });
 
 $router->get('/list', function (Request $request) {
-    return view('list', ['mode' => $request->input('mode', 'favorites')])->render();
+    $mode = $request->input('mode', 'favorites');
+    $favorites = collect();
+
+    if ($mode === 'favorites') {
+        $user = Auth::user();
+        if ($user) {
+            $favorites = $user->favorites()
+                ->with('anime')
+                ->orderByDesc('created_at')
+                ->get();
+        }
+    }
+
+    return view('list', [
+        'mode' => $mode,
+        'favorites' => $favorites,
+    ])->render();
 });
 
 $router->get('/details', function () {
@@ -41,3 +58,5 @@ $router->get('/register', 'AuthController@showRegister');
 $router->post('/register', 'AuthController@register');
 $router->post('/login', 'AuthController@login');
 $router->post('/logout', 'AuthController@logout');
+$router->post('/favorites', 'FavoriteController@store');
+$router->delete('/favorites/{animeId:[0-9]+}', 'FavoriteController@destroy');
