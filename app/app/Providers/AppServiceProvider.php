3c3b2ccdfbef55f2a7b60597ec87cfe598cfcf26
Alexander Mikhailov <45555229+mikhaia@users.noreply.github.com>
2025-09-22 10:49:18 +0700

    Bind catalog service in container

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Support\AnilibriaClient;
+use App\Support\AnimeCatalogService;
+use App\Support\PosterStorage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +16,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(AnilibriaClient::class, function (): AnilibriaClient {
+            return new AnilibriaClient();
+        });
+
+        $this->app->singleton(PosterStorage::class, function (): PosterStorage {
+            return new PosterStorage();
+        });
+
+        $this->app->singleton(AnimeCatalogService::class, function ($app): AnimeCatalogService {
+            return new AnimeCatalogService(
+                $app->make(AnilibriaClient::class),
+                $app->make(PosterStorage::class),
+            );
+        });
     }
 }
