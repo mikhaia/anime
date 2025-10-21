69918974a70bf04162e186cf016a578ad030585d
Alexandr Mikhailov <alexander@enclave.vn>
2025-10-21 13:18:42 +0700

    Fix some sitles

--- app/resources/views/lite/search.blade.php
+++ app/resources/views/lite/search.blade.php
@@ -1,14 +1,14 @@
 @extends('lite.list')
 
 @section('content')
-    <div class="search-page">
-        <div class="search-bar">
-            <form method="get" action="/search" class="search-form">
-                <input type="text" name="query" placeholder="Название аниме" required>
-                <button type="submit">Искать</button>
-            </form>
-        </div>
+<div class="search-page">
+    <div class="search-bar">
+        <form method="get" action="/search" class="search-form">
+            <input type="text" name="query" placeholder="Название аниме" value="{ $searchQuery }}" required>
+            <button type="submit">Искать</button>
+        </form>
     </div>
+</div>
 
-    @parent
+@parent
 @endsection
