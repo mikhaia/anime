54ee60be88803b9a4d2296481af486429db45c29
Alexandr Mikhailov <alexander@enclave.vn>
2025-10-21 13:32:32 +0700

    Fix mistypo

--- app/resources/views/lite/search.blade.php
+++ app/resources/views/lite/search.blade.php
@@ -4,7 +4,7 @@
 <div class="search-page">
     <div class="search-bar">
         <form method="get" action="/search" class="search-form">
-            <input type="text" name="query" placeholder="Название аниме" value="{ $searchQuery }}" required>
+            <input type="text" name="query" placeholder="Название аниме" value="{{ $searchQuery }}" required>
             <button type="submit">Искать</button>
         </form>
     </div>
