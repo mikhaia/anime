7460f9b9181273833bd456885dca748bf786d25f
Alexandr Mikhailov <alexander@enclave.vn>
2025-12-31 09:44:28 +0700

    Fix editing profile for users without avatar

--- app/resources/views/lite/users.blade.php
+++ app/resources/views/lite/users.blade.php
@@ -16,16 +16,15 @@
                     </a>
                     <a href="{{ $url }}">
                         <span class="avatar">
+                            <span class="avatar-edit">
+                                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="200" height="200"
+                                    fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="square" stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M7 19H5a1 1 0 0 1-1-1v-1a3 3 0 0 1 3-3h1m4-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm7.441 1.559a1.907 1.907 0 0 1 0 2.698l-6.069 6.069L10 19l.674-3.372 6.07-6.07a1.907 1.907 0 0 1 2.697 0Z" />
+                                </svg>
+                            </span>
                             @if ($u->avatar_path)
-                                <span class="avatar-edit">
-                                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="200" height="200"
-                                        fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="square" stroke-linejoin="round"
-                                            stroke-width="2"
-                                            d="M7 19H5a1 1 0 0 1-1-1v-1a3 3 0 0 1 3-3h1m4-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm7.441 1.559a1.907 1.907 0 0 1 0 2.698l-6.069 6.069L10 19l.674-3.372 6.07-6.07a1.907 1.907 0 0 1 2.697 0Z" />
-                                    </svg>
-                                </span>
-
                                 <img src="{{ $u->avatar_path }}" alt="{{ $u->name }}" width="200" height="200">
                             @else
                                 <svg width="200" height="200" viewBox="0 0 20 20" fill="currentColor"
